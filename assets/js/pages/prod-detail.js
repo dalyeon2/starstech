c9c7367e6278605644f9d074f2ed569b50fa35eb
dalyeon2 <woohyun2228@gmail.com>
2026-01-06 14:04:58 +0900

    feat: set up prod-detail

---
+++ assets/js/pages/prod-detail.js
@@ -0,0 +1,53 @@
+/**
+ * prod-detail thumbnail switcher
+ */
+(function ($) {
+    'use strict';
+    if (!$) return;
+
+    function initThumbs() {
+        var $galleries = $('.cont1 .gallery');
+        if (!$galleries.length) return;
+
+        $galleries.each(function () {
+            var $gallery = $(this);
+            var $main = $gallery.find('.main').first();
+            var $thumbs = $gallery.find('.thumb');
+            if (!$main.length || !$thumbs.length) return;
+
+            function setActive($thumb) {
+                var src = $thumb.data('full') || $thumb.attr('src');
+                if (!src) return;
+                $thumbs.removeClass('is-active');
+                $thumb.addClass('is-active');
+                $main.attr('src', src);
+                if ($thumb.attr('alt')) {
+                    $main.attr('alt', $thumb.attr('alt'));
+                }
+            }
+
+            var mainSrc = $main.attr('src');
+            var matched = false;
+            $thumbs.each(function () {
+                var $thumb = $(this);
+                var thumbSrc = $thumb.data('full') || $thumb.attr('src');
+                if (thumbSrc && thumbSrc === mainSrc) {
+                    $thumb.addClass('is-active');
+                    matched = true;
+                    return false;
+                }
+                return undefined;
+            });
+            if (!matched) {
+                $thumbs.eq(0).addClass('is-active');
+            }
+
+            $thumbs.off('click.prodDetail').on('click.prodDetail', function (e) {
+                e.preventDefault();
+                setActive($(this));
+            });
+        });
+    }
+
+    $(initThumbs);
+})(window.jQuery);
